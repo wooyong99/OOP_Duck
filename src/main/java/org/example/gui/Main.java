f3363ee459b97bc1c62396b6aceaff00a4f5156e
wooyong99 <wulovesk@naver.com>
2023-12-15 13:41:19 +0900

    feat : MyFrame 생성

--- src/main/java/org/example/gui/Main.java
+++ src/main/java/org/example/gui/Main.java
@@ -1,10 +1,10 @@
-package org.example;
+package org.example.gui;
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("실행");
+        MyFrame f = new MyFrame();
     }
 }
---
+++ src/main/java/org/example/gui/MyFrame.java
@@ -0,0 +1,21 @@
+package org.example.gui;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+
+public class MyFrame extends Frame {
+    public final int FRAME_WIDTH = 800;
+    public final int FRAME_HEIGHT = 600;
+
+
+    public MyFrame() {
+        super();
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        System.out.println("실행");
+    }
+}
